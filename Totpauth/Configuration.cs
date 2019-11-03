6606de4ad5df8eb0374d4f885e1c4c47e55102b2
Lance <>
2019-11-03 16:05:59 +0100

    Initial Commit

---
+++ Totpauth/Configuration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace pGina.Plugin.Totpauth
+{
+    public partial class Configuration : Form
+    {
+        private readonly Gauth _gauth = new Gauth();
+
+        public Configuration()
+        {
+            InitializeComponent();
+        }
+
+        private void Configuration_Load(object sender, EventArgs e)
+        {
+
+            pictureBox1.Load(_gauth.QrCodeUrl);
+
+            this.WindowState = FormWindowState.Minimized;
+
+            this.Show();
+
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
